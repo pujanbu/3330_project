616e50bf28f502ceeb54b1af48af692c2d2ffcb9
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-11-21 18:53:46 -0600

    added buttons

--- static/index.js
+++ static/index.js
@@ -3,6 +3,12 @@ new Vue({
     data() {
         return {
             profile: true,
+
+            currentUser: "Safal Lamsal", //logged in user
+
+            selectedUser: "Safal Lamsal", //selected user for display
+            selectedBio: "Hey my name is Safal Lamsal and this is my profile.",
+
             newPost: ""
         }
     }
